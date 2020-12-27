f76ef4155b5b313f5c13e0467676cd9ec717eda2
ClaudiusMinimus <claudiusminimus@gmail.com>
2020-12-26 23:50:12 -0700

    Create furnace.js

---
+++ minecraft/kubejs/data/easysteel/kubejs/furnace.js
@@ -0,0 +1,12 @@
+// Listen to server recipe event
+onEvent('recipes', event => {
+
+    // Create a function and use that to make things shorter. You can combine multiple actions
+    const multiSmelt = (output, input) => {
+        event.smelting(output, input)
+        event.blasting(output, input)
+    }
+
+    multiSmelt('silentgems:silver_ingot', 'create:crushed_silver_ore')
+
+})
