889c8f76348a71b6ab38edae2359dd0aa4f17c76
ClaudiusMinimus <claudiusminimus@gmail.com>
2021-01-20 14:47:25 -0700

    Update 99mtw_mkServer.sh

--- 99mtw_mkServer.sh
+++ 99mtw_mkServer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #change to acutal build directory if running from main dir
-cd //home/helaman/.local/share/multimc/instances/Ninety-Nine-Mods-on-the-Wall-build/server
+cd //home/helaman/.local/share/multimc/instances/Ninety-Nine-Mods-on-the-Wall-build
 
-/home/helaman/Downloads/Minecraft/modpacks/mkServer/mkServer.sh '99mtw-1.16.5-1.5.0-server' ./server
+/home/helaman/Downloads/Minecraft/modpacks/mkServer/mkServer.sh '99mtw-1.16.5-1.5.0' ./server
